4e42477eaf01c8dc214097b5ca3012c3a1953d4b
sreenidhipriya <sreenidhi.vandeti118@gmail.com>
2025-12-27 15:05:21 +0530

    Add files via upload

---
+++ bitmanipulation.java
@@ -0,0 +1,12 @@
+class bitmanipulation{
+    public static void main(String[] args) {
+        int a=5,b=2;
+        System.out.println(a&b);
+        System.out.println(a|b);
+        System.out.println(~a);
+        System.out.println(a^b);
+        System.out.println(a<<2);
+        System.out.println(b>>1);
+
+    }
+}
